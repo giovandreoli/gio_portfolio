1a39f8cd590a99dfe55398c04f835cdea31275ce
giovandreoli <giovannaluizaandreoli@gmail.com>
2026-01-19 18:42:02 -0300

    "fix: app imports and window context integration"

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Desktop from "./components/Desktop"
+import Window from "./components/Window"
 
-function App() {
-  const [count, setCount] = useState(0)
+import MyComputer from "./components/MyComputer"
+import ProjectsExplorer from "./components/ProjectsExplorer"
+
+import { useWindows } from "./context/WindowContext.jsx"
+
+export default function App() {
+  const { windows } = useWindows()
+
+  function renderWindow(type) {
+    switch (type) {
+      case "computer":
+        return (
+          <Window key={type} title="Meu Computador" windowId={type}>
+            <MyComputer />
+          </Window>
+        )
+
+      case "projects":
+        return (
+          <Window key={type} title="Projetos" windowId={type}>
+            <ProjectsExplorer />
+          </Window>
+        )
+
+      default:
+        return null
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Desktop />
+      {windows.map(renderWindow)}
     </>
   )
 }
-
-export default App
